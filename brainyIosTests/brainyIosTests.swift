956c04e1a465dfcc4ddd7b9c3eab6101da27f104
kyungseok.lee <lks574@naver.com>
2025-07-31 11:53:51 +0900

    feat: UnitTest 추가

---
+++ brainyIosTests/brainyIosTests.swift
@@ -0,0 +1,16 @@
+//
+//  brainyIosTests.swift
+//  brainyIosTests
+//
+//  Created by KyungSeok Lee on 7/31/25.
+//
+
+import Testing
+
+struct brainyIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
